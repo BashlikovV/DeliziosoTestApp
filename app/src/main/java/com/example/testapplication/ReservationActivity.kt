cf838f9c5f7946d3ae7f37aa07318f94550a10e1
BashlikovV <volodia413314@gmail.com>
2023-01-08 00:25:01 +0300

    Reservation updated, sharedPereferences in ReservationActivity.kt added.

--- app/src/main/java/com/example/testapplication/ReservationActivity.kt
+++ app/src/main/java/com/example/testapplication/ReservationActivity.kt
@@ -1,22 +1,37 @@
 package com.example.testapplication
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testapplication.ui.theme.TestApplicationTheme
-import com.example.testapplication.homepage.TopNavBar
 import com.example.testapplication.reservation.ReservationCard
 import com.example.testapplication.ui.theme.background
 
 class ReservationActivity : ComponentActivity() {
+    private lateinit var sPref: SharedPreferences
+
+    companion object Keys {
+        const val DATE = "data"
+        const val TIME = "time"
+        const val PARTY_SIZE = "party_size"
+    }
+
+    object Data {
+        var date = ""
+        var time = ""
+        var partySize = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TestApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -29,6 +44,43 @@ class ReservationActivity : ComponentActivity() {
                 }
             }
         }
+
+        loadInputData()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveInputData()
+    }
+
+    private fun saveInputData() {
+        sPref = getPreferences(MODE_PRIVATE)
+        val editor = sPref.edit()
+
+        with(editor) {
+            putString(DATE, Data.date)
+            putString(TIME, Data.time)
+            putString(PARTY_SIZE, Data.partySize)
+        }
+        editor.apply()
+
+        Log.i("MYTAGS", Data.date)
+        Log.i("MYTAGS", Data.time)
+        Log.i("MYTAGS", Data.partySize)
+    }
+
+    private fun loadInputData() {
+        sPref = getPreferences(MODE_PRIVATE)
+
+        with(sPref) {
+            Data.date = getString(DATE, "").toString()
+            Data.time = getString(TIME, "").toString()
+            Data.partySize = getString(PARTY_SIZE, "").toString()
+        }
+
+        Log.i("MYTAGL", Data.date)
+        Log.i("MYTAGL", Data.time)
+        Log.i("MYTAGL", Data.partySize)
     }
 }
 
@@ -43,6 +95,5 @@ fun Test() {
         ) {
             ReservationCard(data = "ReservationActivity")
         }
-
     }
 }
--- app/src/main/java/com/example/testapplication/reservation/Reservation.kt
+++ app/src/main/java/com/example/testapplication/reservation/Reservation.kt
@@ -1,37 +1,66 @@
 package com.example.testapplication.reservation
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testapplication.R
+import com.example.testapplication.ReservationActivity
 import com.example.testapplication.footer.Footer
 import com.example.testapplication.homepage.TopNavBar
 import com.example.testapplication.ui.theme.cuisineColor
+import com.example.testapplication.ui.theme.inputColor
 
 @Composable
 fun ReservationCard(data: String) {
+    fun saveInputData(index: Int, str: String) {
+        when(index) {
+            0 -> {
+                ReservationActivity.Data.date = str
+                Log.i("MYTAG", "data: ${ReservationActivity.Data.date}")
+            }
+            1 -> {
+                ReservationActivity.Data.time = str
+                Log.i("MYTAG", "time: ${ReservationActivity.Data.time}")
+            }
+            2 -> {
+                ReservationActivity.Data.partySize = str
+                Log.i("MYTAG", "party size: ${ReservationActivity.Data.partySize}")
+            }
+        }
+    }
+
     Column(
         modifier = Modifier
             .verticalScroll(
                 state = rememberScrollState(),
                 enabled = true
             )
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TopNavBar(name = data)
-        Row(
-            horizontalArrangement = Arrangement.Center
-        ) {
+        Row {
             Text(
                 text = "Book a table",
                 fontSize = 30.sp,
@@ -46,15 +75,68 @@ fun ReservationCard(data: String) {
                 painter = painterResource(id = R.drawable.component),
                 contentDescription = "Component",
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 2.dp)
+                    .fillMaxHeight(1f)
+                    .padding(top = 30.dp, bottom = 30.dp)
                     .size(
                         width = 353.94.dp,
                         height = 256.29.dp
                     )
+                    .offset(x = (-60).dp),
+                contentScale = ContentScale.FillHeight,
             )
         }
+        GetText(value = "Date") {
+            saveInputData(0, it)
+        }
+        GetText(value = "Time") {
+            saveInputData(1, it)
+        }
+        GetText(value = "Party size") {
+            saveInputData(2, it)
+        }
         Footer(name = data)
+    }
+}
+
+@Composable
+fun GetText(value: String, listener: (String) -> Unit) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .padding(bottom = 25.dp)
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+                listener(text.text)
+            },
+            label = {
+                Text(
+                    text = value,
+                    color = Color.Black,
+                    fontSize = 12.sp
+                )
+            },
+            modifier = Modifier
+                .clip(RoundedCornerShape(20))
+                .onSizeChanged {
+                    when(value[0]) {
+                        'D' -> text = TextFieldValue(ReservationActivity.Data.date)
+                        'T' -> text = TextFieldValue(ReservationActivity.Data.time)
+                        'P' -> text = TextFieldValue(ReservationActivity.Data.partySize)
+                    }
+                },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = null
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(inputColor),
+        )
     }
 }
 
