68b58d65797211a9a033af456b0b5122a5fdef6e
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-01-27 21:48:27 +0300

    Menu navigation screens redone.

--- app/src/main/java/com/example/testapplication/menu/MenuThirdScreen.kt
+++ app/src/main/java/com/example/testapplication/menu/MenuThirdScreen.kt
@@ -1,8 +1,9 @@
 package com.example.testapplication.menu
 
 import androidx.compose.runtime.Composable
+import com.example.testapplication.homepage.HeadBody
 
 @Composable
 fun MenuThirdScreen() {
-
+    HeadBody()
 }
