b2bf4782698a7ac88ea342f9fa200d6100fbf4ef
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-03 23:46:38 +0300

    ReservationActivity code cleaned (onSaveInstanceState remade to viewModel).

--- app/src/main/java/com/example/testapplication/ReservationActivity.kt
+++ app/src/main/java/com/example/testapplication/ReservationActivity.kt
@@ -3,7 +3,6 @@ package com.example.testapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -12,10 +11,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.testapplication.reservation.ReservationCard
 import com.example.testapplication.ui.theme.TestApplicationTheme
 import com.example.testapplication.ui.theme.background
-import com.example.testapplication.view.model.ReservationActivityViewModel
 
 class ReservationActivity : ComponentActivity() {
-    private val viewModel by viewModels<ReservationActivityViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +25,11 @@ class ReservationActivity : ComponentActivity() {
                     color = background,
                 ) {
                     ReservationCard(
-                        name = this.localClassName,
-                        reservationActivityViewModel = viewModel
+                        name = this.localClassName
                     )
                 }
             }
         }
-
-        viewModel.initState(
-            savedInstanceState?.getParcelable(KEY) ?: ReservationActivityViewModel
-                .State("", "", "")
-        )
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable(KEY, viewModel.state.value)
-    }
-
-    companion object {
-        @JvmStatic val KEY = "KEY"
     }
 }
 
@@ -60,7 +42,7 @@ fun Test() {
                 .fillMaxSize(),
             color = background,
         ) {
-            ReservationCard(name = "ReservationActivity", ReservationActivityViewModel())
+            ReservationCard(name = "ReservationActivity")
         }
     }
 }
