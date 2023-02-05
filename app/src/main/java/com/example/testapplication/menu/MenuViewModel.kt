0ddddf9eae51bcda1743a332d07ae6079a928406
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-05 22:21:18 +0300

    Code cleaned.

--- app/src/main/java/com/example/testapplication/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/testapplication/menu/MenuViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.testapplication.menu.product.view.ProductView
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
