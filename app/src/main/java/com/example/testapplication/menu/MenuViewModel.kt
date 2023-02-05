bc90c2fa7f0160bc36b0a29838a2dfc2c323fe3e
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-05 18:24:07 +0300

    MenuViewModel.kt updated.

--- app/src/main/java/com/example/testapplication/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/testapplication/menu/MenuViewModel.kt
@@ -1,36 +1,60 @@
 package com.example.testapplication.menu
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-data class MenuUiState(
-    var counts: List<Int> = listOf(0),
-    val images: List<Int>? = null,
-    val names: List<String>? = null
-)
+/**
+ * [MenuViewModel] contains data for [MenuFirstScreen]
+ * that contains LazyHorizontalGrid with [ProductView]
+ * */
 
-class MenuUiViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(MenuUiState())
-    val uiState: StateFlow<MenuUiState> = _uiState.asStateFlow()
+class MenuViewModel: ViewModel() {
+    private val _menuUiState = MutableStateFlow(listOf(MenuUiState()))
+    val menuUiState: StateFlow<List<MenuUiState>> = _menuUiState.asStateFlow()
+
+    var selectedItem by mutableStateOf(0)
+        private set
 
     init {
-        val data = MenuActivityData().testData
-        setUiState(
-            counts = data.map { it.countValue },
-            images = data.map { it.imageValue },
-            names = data.map { it.productNameValue }
-        )
+        initMenuUiState()
     }
 
-    private fun setUiState(counts: List<Int>, images: List<Int>?, names: List<String>?) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                counts = counts,
-                images = images,
-                names = names
+    private fun initMenuUiState() {
+        _menuUiState.update {
+            (0..5).map {
+                MenuUiState(
+                    currentImageValue = MenuActivityData.imagesIds[it],
+                    currentNameValue = MenuActivityData.namesList[it],
+                    currentCostValue = (10..100).random().toFloat(),
+                    currentStarsValue = (1..5).random(),
+                    currentIsSelectedValue = false,
+                    currentCountValue = 0,
+                    index = it
+                )
+            }
+        }
+    }
+
+    fun onSelectItem(index: Int) {
+        selectedItem = index
+    }
+
+    fun onCountChanged(index: Int, newValue: Int) {
+        _menuUiState.update {
+            updateCountItemValue(index, newValue)
+        }
+    }
+
+    private fun updateCountItemValue(index: Int, newValue: Int): List<MenuUiState> {
+        return _menuUiState.value.map {
+            it.copy(
+                currentCountValue = if (it.index == index) newValue else it.currentCountValue
             )
         }
     }
